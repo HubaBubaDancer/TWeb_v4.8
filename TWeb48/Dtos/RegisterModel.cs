2b0a57fce86a1ef053684c8785a631d87fef6bca
HubaBubaDancer <vladik.dolghier@gmail.com>
2025-05-27 20:29:25 +0300

    fix: deleting stuff

--- TWeb48/Dtos/RegisterModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TWeb48.Models
-{
-    public class RegisterModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
-}
